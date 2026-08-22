d7e17774dc7f8e0c859ff3f876f9f3c2115ac056
hrqandrade <35543009+hrqandrade@users.noreply.github.com>
2026-08-22 17:34:44 -0300

    fix: register the application scene delegate

--- NestlyApp/App/AppDelegate.swift
+++ NestlyApp/App/AppDelegate.swift
@@ -7,7 +7,11 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         configurationForConnecting connectingSceneSession: UISceneSession,
         options: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
-        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        let configuration = UISceneConfiguration(
+            name: "Default Configuration",
+            sessionRole: connectingSceneSession.role
+        )
+        configuration.delegateClass = SceneDelegate.self
+        return configuration
     }
 }
-
